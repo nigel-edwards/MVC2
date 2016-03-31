0bfc6db655a23cf84592847d0ac2bd33aa7d12df
nigel-edwards <nigel.edwards@cwa.ac.uk>
2016-03-31 21:07:30 +0100

    Added a simple model

--- MVC2/Controllers/TestController.cs
+++ MVC2/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,18 @@ namespace MVC2.Controllers
         public ActionResult NewStuff()
         {
             return View("NewStuff");
+        }
+
+        public ActionResult GetView()
+        {
+            Customer cust = new Customer();
+            cust.FirstName = "Debbie";
+            cust.LastName = "Edwards";
+            cust.Salary = 200000;
+
+            ViewData["Customer"] = cust;
+
+            return View("GetView");
         }
 	}
 }
---
+++ MVC2/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2.Models
+{
+    public class Customer
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Salary { get; set; }
+    }
+}
