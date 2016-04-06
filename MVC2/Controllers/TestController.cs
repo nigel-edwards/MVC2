ab63f7031688ba0f3448b1bc008dd6f1ad2cace8
nigel-edwards <nigel.edwards@cwa.ac.uk>
2016-04-06 20:31:14 +0100

    Added Collection VM

--- MVC2/Controllers/TestController.cs
+++ MVC2/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC2.ViewModels;
 
 namespace MVC2.Controllers
 {
@@ -23,14 +24,35 @@ namespace MVC2.Controllers
 
         public ActionResult GetView()
         {
-            Customer cust = new Customer();
-            cust.FirstName = "Debbie";
-            cust.LastName = "Edwards";
-            cust.Salary = 1000;
+            CustomerListViewModel customersVM = new CustomerListViewModel();
 
-            ViewData["Customer"] = cust;
-            ViewBag.Customer = cust;
-            return View("GetView", cust);
+            CustomerBuisnessLayer customerBuisLayer = new CustomerBuisnessLayer();
+            List<Customer> customers = customerBuisLayer.GetCustomers();
+            List<CustomerViewModel> custViewModels = new List<CustomerViewModel>();
+
+            foreach (Customer cust in customers )
+            {
+                CustomerViewModel vmCus = new CustomerViewModel();
+                vmCus.CustomerName = cust.FirstName + " " + cust.LastName;
+                vmCus.Salary = cust.Salary.ToString("C");
+                if (cust.Salary > 15000)
+                {
+                    vmCus.SalaryColor = "yellow";
+                }
+                else
+                {
+                    vmCus.SalaryColor = "green";
+                }
+                custViewModels.Add(vmCus);
+            }
+
+
+            customersVM.UserName = "Admin";
+            customersVM.Customers = custViewModels;
+
+
+
+            return View("GetView", customersVM);
         }
 	}
 }
---
+++ MVC2/Models/CustomerBuisnessLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2.Models
+{
+    public class CustomerBuisnessLayer
+    {
+        public List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            Customer cus = new Customer();
+            cus.FirstName = "Johnson";
+            cus.LastName = " Fernandes";
+            cus.Salary = 14000;
+            customers.Add(cus);
+
+            cus = new Customer();
+            cus.FirstName = "Deborah";
+            cus.LastName = " Edwards";
+            cus.Salary = 54000;
+            customers.Add(cus);
+
+            cus = new Customer();
+            cus.FirstName = "Nigel";
+            cus.LastName = " Edwards";
+            cus.Salary = 34000;
+            customers.Add(cus);
+
+            return customers;
+        }
+    }
+}
