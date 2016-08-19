006048cef4639cf51a8c1769d3e8374385778b77
Nigel <nigel.edwards@cwa.ac.uk>
2016-08-19 08:13:07 +0100

    Added all untracked files

---
+++ MVC2/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public string CustomerName { get; set; }
+        public string Salary { get; set; }
+        public string SalaryColor { get; set; }
+    }
+
+    public class CustomerListViewModel
+    {
+        public List<CustomerViewModel> Customers { get; set; }
+        public string UserName { get; set; }
+    }
+
+}
